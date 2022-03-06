d196ebd62554a31efdd947ba75310a5dac336279
Stepan <St3Ba4@gmail.com>
2022-03-06 11:23:24 +0300

    add gui

---
+++ gui.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+import tkinter.filedialog as fd
+
+def get_adress():
+	path = fd.Open(filetypes = [('*.txt files', '.txt'), ('*.csv files', '.csv')]).show()
+	return path
+
+def get_adress_to_save():
+	root = tk.Tk()
+	path = fd.SaveAs(root, filetypes = [('*.txt files', '.txt'), ('*.csv files', '.csv')]).show()
+	return path
+
+# def textarea():
+# 	textFrame = tk.Frame(height = 340, width = 600)
+# 	textFrame.grid(row = 2, column = 1)
+# 	textbox = tk.Text(textFrame, font='Arial 14', wrap='word')
+# 	scrollbar = tk.Scrollbar(textFrame)
+# 	scrollbar['command'] = textbox.yview
+# 	textbox['yscrollcommand'] = scrollbar.set
